a382b98dad34f2858bb55a3885d6b38e557bf271
bhoon-coding <bhooncoding@gmail.com>
2023-06-15 09:51:10 +0900

    attached macro (구현부 수정 & macro 선언부 수정)

--- Sources/EnumSubset/EnumSubset.swift
+++ Sources/EnumSubset/EnumSubset.swift
@@ -3,9 +3,6 @@
 
 /// A macro that produces both a value and a string containing the
 /// source code that generated the value. For example,
-///
-///     #stringify(x + y)
-///
-/// produces a tuple `(x + y, "x + y")`.
-@freestanding(expression)
-public macro stringify<T>(_ value: T) -> (T, String) = #externalMacro(module: "EnumSubsetMacros", type: "StringifyMacro")
+
+@attached(member, names: named(init))
+public macro EnumSubset() = #externalMacro(module: "EnumSubsetMacros", type: "EnumSubsetMacro")
